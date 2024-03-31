897067043e30c93dc48c8672bfdacee227c10ab2
WillarDraco <a1863235@adelaide.edu.au>
2024-04-01 00:33:22 +1030

    Update -1

--- Finder.cpp
+++ Finder.cpp
@@ -5,9 +5,13 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
+        int seen = 0;
 
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
+            size_t found = s1.find(s2.substr(0, i), seen);
+
+            seen = found;
+
             if (found != string::npos) {
                 result.push_back(found);
             } else {
