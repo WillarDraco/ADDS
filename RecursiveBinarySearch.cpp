475735a3dcc69c81b76f2b265ee5f210a6eb624b
WillarDraco <a1863235@adelaide.edu.au>
2024-04-22 01:19:13 +0930

    RBS fix.2

--- RecursiveBinarySearch.cpp
+++ RecursiveBinarySearch.cpp
@@ -2,16 +2,17 @@
 
 bool RecursiveBinarySearch::search(std::vector<int> array, int target) {
     int middle = array.size() / 2;
+    std::cout << middle << std::endl;
     
     if (array.size() == 1 && array[0] != target) {
         return false;
     }
 
-    if (array[middle] < target) {
+    if (array[middle] == target) {
+        return true;
+    } else if (array[middle] < target) {
         return search(std::vector<int>{array.begin(), array.begin() + middle}, target);
     } else if (array[middle] > target) {
         return search(std::vector<int>{array.begin()+ middle + 1, array.end()},target);
-    } else if (array[middle] == target) {
-        return true;
-    }
+    } 
 }
